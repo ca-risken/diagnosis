d483bd0105760d2d0afee4d5ca28a3f38ec25c29
iiiidaaa <35591487+iiiidaaa@users.noreply.github.com>
2020-10-12 15:05:21 +0900

    Update message.go

--- pkg/message/message.go
+++ pkg/message/message.go
@@ -3,6 +3,7 @@ package message
 // DiagnosisQueueMessage is the message for SQS queue
 type DiagnosisQueueMessage struct {
 	DataSource    string `json:"data_source"`
+	JiraSettingID uint32 `json:"jira_setting_id"`
 	ProjectID     uint32 `json:"project_id"`
 	IdentityField string `json:"identity_field"`
 	IdentityValue string `json:"identity_value"`
