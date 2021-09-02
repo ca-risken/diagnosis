ccf1870bcef9df725856f397aca84713c3df6af2
iiiidaaa <oeriver@gmail.com>
2021-09-02 12:50:56 +0900

    add application scan message

--- pkg/message/message.go
+++ pkg/message/message.go
@@ -30,3 +30,13 @@ type PortscanQueueMessage struct {
 	Target            string `json:"target"`
 	ScanOnly          bool   `json:"scan_only,string"`
 }
+
+// ApplicationScanQueueMessage is the message for SQS queue for ApplicationScan
+type ApplicationScanQueueMessage struct {
+	DataSource          string `json:"data_source"`
+	ApplicationScanID   uint32 `json:"application_scan_id"`
+	ProjectID           uint32 `json:"project_id"`
+	Target              string `json:"target"`
+	ApplicationScanType string `json:"application_scan_type"`
+	ScanOnly            bool   `json:"scan_only,string"`
+}
