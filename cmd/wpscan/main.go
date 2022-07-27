d835bc1d55a60637b0456abbba4ab0224a1c9df8
mmmknt <mmmknt@mmmknt.dev>
2022-07-27 20:17:28 +0900

    Return error when failed to create sqs consumer

--- cmd/wpscan/main.go
+++ cmd/wpscan/main.go
@@ -127,7 +127,10 @@ func main() {
 		MaxNumberOfMessage: conf.MaxNumberOfMessage,
 		WaitTimeSecond:     conf.WaitTimeSecond,
 	}
-	consumer := newSQSConsumer(ctx, sqsConf)
+	consumer, err := newSQSConsumer(ctx, sqsConf)
+	if err != nil {
+		appLogger.Fatalf(ctx, "Failed to create SQS consumer, err=%+v", err)
+	}
 	appLogger.Info(ctx, "Start the wpscan SQS consumer server...")
 	consumer.Start(ctx,
 		mimosasqs.InitializeHandler(
