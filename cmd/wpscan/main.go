f9189f09f3aab41c3e26a73d27abc97465a0334b
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-05-09 11:42:05 +0900

    fix: dataousource api

--- cmd/wpscan/main.go
+++ cmd/wpscan/main.go
@@ -43,7 +43,7 @@ type AppConfig struct {
 	WaitTimeSecond           int32  `split_words:"true" default:"20"`
 	// grpc
 	CoreAddr             string `required:"true" split_words:"true" default:"core.core.svc.cluster.local:8080"`
-	DataSourceAPISvcAddr string `required:"true" split_words:"true" default:"datasource-api.core.svc.cluster.local:8081"`
+	DataSourceAPISvcAddr string `required:"true" split_words:"true" default:"datasource-api.datasource.svc.cluster.local:8081"`
 	// wpscan
 	ResultPath string `split_words:"true" required:"true" default:"/tmp"`
 }
